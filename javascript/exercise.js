547fc287b453c040761af512732618e7c5d16fc5
Rudra2637 <145354880+Rudra2637@users.noreply.github.com>
2024-11-17 18:30:47 +0000

    Updated the code for object as learned new things

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -1,2 +1,23 @@
-console.log("hello");
-console.log("my first commit and push on github");
+/*console.log("hello");
+console.log("my first commit and push on github");*/
+const obj1={
+    name:"rudra",
+    age:19,
+    loggedIn:"true",
+    prop:{
+        isValid:"true",
+        location:"pune"
+    }
+}
+const greet=function(){
+    console.log('hey its a pleasure to meet '+obj1.name);
+}
+console.log(greet());
+const obj2={1:"a",2:"b"}
+const obj3={3:"c",4:"5"}
+
+const obj4={...obj2,...obj3};
+console.log(obj4);
+
+console.log(Object.values(obj1.prop));
+console.log(obj1.hasOwnProperty("logged"));
