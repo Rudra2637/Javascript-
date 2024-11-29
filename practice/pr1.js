6dab0c8a91e17072624fffa376795aaae4661693
Rudra2637 <145354880+Rudra2637@users.noreply.github.com>
2024-11-29 14:23:29 +0000

    Revised javaScript concepts and Dom

---
+++ practice/pr1.js
@@ -0,0 +1,19 @@
+// console.log("hello world")
+const obj=[{
+    name:"rudra",
+    age:19,
+    year:2004
+},
+{
+    name:"Sujal",
+    age:20,
+    year:2004
+
+}
+]
+
+for (const num of obj){
+  //  console.log(num)
+    console.log(num.name)
+    
+}
